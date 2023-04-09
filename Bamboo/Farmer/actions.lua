2bb2e2ce027b04b1556611827b80557c7918ce08
ErrorDodo <midgetmenarecool@gmail.com>
2023-04-09 15:15:57 +0800

    Add 2 actions for turtle to do

---
+++ Bamboo/Farmer/actions.lua
@@ -0,0 +1,64 @@
+Directions = {
+    front = function() end,
+    top = function() end,
+    back = function() end,
+    left = function() end,
+    right = function() end
+}
+
+-- To use function CheckFaceForBlock("front")
+function CheckFaceForBlock(dir)
+    local result
+    case = {
+      ["front"] = function() result = turtle.detect() end,
+      ["top"] = function() result = turtle.detectUp() end,
+      ["back"] = function() result = turtle.detectDown() end,
+      ["left"] = function()
+        turtle.turnLeft()
+        result = turtle.detect()
+        turtle.turnRight()
+      end,
+      ["right"] = function()
+        turtle.turnRight()
+        result = turtle.detect()
+        turtle.turnLeft()
+      end
+    }
+    local detectFunc = case[dir]
+    if detectFunc then
+      detectFunc()
+    end
+    return result or false
+end
+
+-- To use function Move("forward", 2)
+function Move(dir, numBlocks)
+    numBlocks = numBlocks or 1
+    local result
+    case = {
+      ["forward"] = function() result = turtle.forward() end,
+      ["up"] = function() result = turtle.up() end,
+      ["back"] = function() result = turtle.back() end,
+      ["down"] = function() result = turtle.down() end,
+      ["left"] = function()
+        turtle.turnLeft()
+        result = turtle.forward()
+        turtle.turnRight()
+      end,
+      ["right"] = function()
+        turtle.turnRight()
+        result = turtle.forward()
+        turtle.turnLeft()
+      end
+    }
+    local moveFunc = case[dir]
+    if moveFunc then
+        for i=1,numBlocks do
+            moveFunc()
+            if not result then
+                break
+            end
+        end
+    end
+    return result
+end
